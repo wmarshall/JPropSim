139d65013b5754516d95f8d6f06558393742a942
Will Marshall <willcodymarshall@gmail.com>
2016-05-30 17:37:01 -0400

    initial gui work

---
+++ src/com/wcmarshall/jpropsim/SimGui.java
@@ -0,0 +1,16 @@
+package com.wcmarshall.jpropsim;
+
+import javax.swing.*;
+
+/**
+ * Created by wm on 5/29/16.
+ */
+public class SimGui {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextArea RawBytes;
+    private JTextArea DisasmLongs;
+    private JScrollPane HubRamView;
+    private JSplitPane RawDisasmPane;
+    private JScrollBar scrollBar1;
+}
